0d766f35d2acb4e96f53b2100b0b433e6c0a74d2
malaminium <98373120+malaminium@users.noreply.github.com>
2022-02-09 13:42:25 -0500

    Add files via upload

---
+++ test run.js
@@ -0,0 +1,9 @@
+//lets do some js practicing
+// function manifestoToggle() {
+//     var x = document.getElementById(maniTog);
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
